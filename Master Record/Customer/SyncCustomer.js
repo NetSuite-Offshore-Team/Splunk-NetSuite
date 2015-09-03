40f626f2fa358033b20e485bdc012772ed711cc8
Vaibhav <vaibhav.a.srivastava@accenture.com>
2015-09-03 18:34:40 +0530

    Vaibhav Change 9/3/2015

--- Master Record/Customer/SyncCustomer.js
+++ Master Record/Customer/SyncCustomer.js
@@ -27,6 +27,10 @@ function SyncCustomer(type){
         //Vab Change GitHub
 //New Change New Day
         //Vab Change Eclipse
+        /*---------Start Change 9/3/2015-------*/
+        var pqr = 123;
+        /*---------End Change 9/3/2015-------*/
+        
         /*------New Change 8/19-------*/
         
        var test = 1234
