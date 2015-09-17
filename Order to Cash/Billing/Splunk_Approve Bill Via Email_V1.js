ab10e27e9dcabf7686c9da1865fa1c3f3aeb23b0
Vaibhav <vaibhav.a.srivastava@accenture.com>
2015-09-17 20:26:01 +0530

    Committing the new changes

--- Order to Cash/Billing/Splunk_Approve Bill Via Email_V1.js
+++ Order to Cash/Billing/Splunk_Approve Bill Via Email_V1.js
@@ -25,15 +25,25 @@
 * @Project # DFCT0050493
 * @version 1.2
 */
-//Modifying the code logic as a part of fix due to deprecation of "Period Subsidiary" Saved Search. Function "getPeriodofSubsidiary" is no longer used in script. The script will be executed from Release Manager Role now 
+//Modified the code to run the script from Release Manager Role. 
 /**
 * Getting rid of the method getperiodofSubsidiary 
 * @author Vaibhav Srivastava
-* @Release Date: 11th Sep 2015
-* @Release Number : RLSE0050432
-* @Project ENHC0052043
+* @Release Date: 12th Sep 2015
+* @Release Number : ENHC0052043
+* @Project 
 * @version 1.3
 */
+//Changing the script logic again due to NS Defect in loading Vendor Record from Release Manager role in Production
+/**
+* Modifying the logic to accomodate "getPeriodofSubsidiary" function again. Using the new search "Task Item Status" to replace deprecated search "Period Subsidiary"
+* @author Vaibhav Srivastava
+* @Release Date: 17th Sep 2015
+* @Release Number : 
+* @Project 
+* @version 1.4
+*/ 
+
 
 var finalApprover = nlapiGetContext().getSetting('SCRIPT', 'custscript_spk_finalapprover');
 var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
@@ -98,14 +108,20 @@ function approveBill(request,response)
 						}
 						else
 						{
+							var period = currentRec.getFieldText('postingperiod');	// Getting the posting period
+							var periodintid = currentRec.getFieldValue('postingperiod'); //Adding Posting Period internal id as the Posting Period text can not be used as a filter in the New Saved Search "Task Item Status Search"
+							nlapiLogExecution('DEBUG','Test Period Id',periodintid);
+							var cdate1 = new Date();
+							var year = cdate1.getFullYear();						
+							getperiodofSubsidiary(currentRec,year,period,custVBId,periodintid);
+							
+							// After Final Approval the approval status is set to "Approved" and Next Approver as "Fully Approved". 
 							currentRec.setFieldValue('approvalstatus','2');
 							currentRec.setFieldValue('custbody_spk_inv_apvr',finalApprover);
-
 							if(custVBId)
 							{
 								var customVbId = nlapiSubmitField('customrecord_spk_vendorbill',custVBId,['custrecord_spk_vb_approver','custrecord_spk_vb_approvalstatus'],[finalApprover,'2']);
-								var custId = nlapiSubmitField('customrecord_spk_vendorbill',custVBId,'custrecord_spk_vb_postingperiod',currentRec.getFieldValue('postingperiod'));
-								nlapiLogExecution('DEBUG', 'customVbId is', custVBId);
+								nlapiLogExecution('DEBUG', 'customVbId is', customVbId);
 							}
 						}	
 						flag = 1;					
@@ -180,7 +196,100 @@ function approveBill(request,response)
     }
 }
 //AP Approval Phase 2 : End
+/*This function gets the appropriate posting period depending on the role of the user*/
+function getperiodofSubsidiary(rec,year,period,custVBId,perintid)
+{
+	/**** Creating search for finding the open posting period ****/
+	var istrue = 0;
+	var subsidiary = rec.getFieldValue('subsidiary');
+	nlapiLogExecution('DEBUG', 'year '+year, ' period '+ period+' subsidiary '+subsidiary+' PeriodIntid '+perintid);
+	var filter = new Array();
+	var column = new Array();
+	filter[0] = new nlobjSearchFilter('subsidiary',null,'anyof',subsidiary);
+	filter[1] = new nlobjSearchFilter('period','null','is',perintid);
+	column[0] = new nlobjSearchColumn('internalid');
+	//New "Task Item Status" search to be used to replace the deprecated "Period Subsidiary" search
+	var result = nlapiSearchRecord(null,'customsearch_spk_task_item_status_search',filter, column);
+	//nlapiLogExecution('DEBUG','result length',result.length);
+	if(result) {
+		for(var x=0; x<result.length;x++) {
+			var itemtype = result[x].getValue('itemtype');
+			nlapiLogExecution('DEBUG','Item Type',itemtype);
+			if(itemtype == "Lock A/P")
+				{
+				var APLockStatus = result[x].getValue('complete');
+				nlapiLogExecution('DEBUG','APLockStatus',APLockStatus);
+				if(APLockStatus == 'F')
+					{
+					
+					var filter1 = new Array();
+					var column1 = new Array();
 
+					filter1[0] = new nlobjSearchFilter('internalid',null,'anyof',perintid);
+					column1[0] = new nlobjSearchColumn('internalid');
+					var Accresult = nlapiSearchRecord('accountingperiod',null,filter1, column1);
+					if(Accresult) {
+						var accid = Accresult[0].getValue('internalid');
+						nlapiLogExecution('DEBUG', 'accid '+accid,' Accresult '+Accresult.length);
+						if(custVBId) {
+							nlapiSubmitField('customrecord_spk_vendorbill',custVBId,'custrecord_spk_vb_postingperiod',accid);
+						}
+						rec.setFieldValue('postingperiod',accid); 	// Setting of the posting period in bill record.
+						istrue = 1;
+						}
+					}
+				}
+			}
+		}
+		/*------Start---This part of the code is implemented so that the future dated period that are not there in "Task Item Search" can be successfully used in AP Bill---Start------*/
+		else{
+		nlapiLogExecution('DEBUG','The period is outside task item search','T');
+			var filter1 = new Array();
+			var column1 = new Array();
+
+			filter1[0] = new nlobjSearchFilter('internalid',null,'anyof',perintid);
+			column1[0] = new nlobjSearchColumn('internalid');
+			var Accresult = nlapiSearchRecord('accountingperiod',null,filter1, column1);
+			if(Accresult) {
+				var accid = Accresult[0].getValue('internalid');
+				nlapiLogExecution('DEBUG', 'accid '+accid,' Accresult '+Accresult.length);
+				if(custVBId) {
+					nlapiSubmitField('customrecord_spk_vendorbill',custVBId,'custrecord_spk_vb_postingperiod',accid);
+				}
+			rec.setFieldValue('postingperiod',accid); 	// Setting of the posting period in bill record.
+			istrue = 1;
+			}
+		}
+		/*------End---This part of the code is implemented so that the future dated period that are not there in "Task Item Search" can be successfully used in AP Bill---End------*/	
+	if(istrue == 0) {			
+		
+		var txtPeriod = period.split(' ');					
+		var monthIndex = monthNames.indexOf(txtPeriod[0]);	
+		var periodinternalid = '';
+//		Added line of code logic for year captured from Posting period name due to the issue with Vendor Bill Posting for 2016 (INC0090657) ie was due to current system year	: Begin	
+		year = parseInt(txtPeriod[1]);
+		//END		
+		monthIndex = monthIndex + 1;
+		nlapiLogExecution('DEBUG', 'monthIndex', monthIndex +'monthNames[monthIndex]'+ monthNames[monthIndex]+' year  '+year); 
+		if(monthIndex == 12)
+		{
+			monthIndex = 0;
+			year = parseInt(year+1);
+		}
+		var periodname = monthNames[monthIndex] +' '+ year;
+		var filter2 = new Array();
+        filter2[0] = new nlobjSearchFilter('periodname',null,'is',periodname);
+		/*-------New Change as per Enhancement#---------*/
+        var newperiodid = nlapiSearchRecord('accountingperiod','customsearch_spk_act_per_search_apapprov',filter2);
+        for (var p in newperiodid)
+			{
+			periodinternalid = newperiodid[p].getValue('internalid');
+			nlapiLogExecution('DEBUG','New Period Internal ID',periodinternalid);
+			getperiodofSubsidiary(rec,year,periodname,custVBId,periodinternalid);	
+			}
+		/*-----End of New Change as per Enhancement#-----*/
+	}	
+}
 
 // Function to check if any delegate approver is assigned to an employee record.
 function getDelegateApprover(approverId)
